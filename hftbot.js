32ce6aa55b73d4b5b8f953ad52552928ea6ffc97
baimao <baimao@xfers.com>
2018-10-03 02:48:14 +0800

    Add place order

--- hftbot.js
+++ hftbot.js
@@ -1,12 +1,14 @@
 const key = require('./secret')
+const Decimal = require('decimal.js')
 
 const Client = require('./index')
 let client = new Client({ key: key })
 const PAIR = 'ETH-USDT'
 const ORDER_SIZE = '0.1'
 
-let highestBid = {price: 0, size: 0}
-let lowestAsk = {price: 1000000, size: 0}
+let highestBid = {price: new Decimal(0), size: new Decimal(0)}
+let lowestAsk = {price: new Decimal(1000000), size: new Decimal(0)}
+let lastTrade = {price: null, size: null}
 let myOrderPair = {
   ask:{
     price: null,
@@ -20,48 +22,58 @@ let myOrderPair = {
 }
 
 function setBid(bid){
-  let [price, type, size] = bid
-  price = parseFloat(price).toFixed(2)
-  type = type == 1 ? 'add' : 'remove'
-  // console.log('bid', price, type, size)
-  if(parseFloat(price) > parseFloat(highestBid.price) && type == 'add'){
-    highestBid = {price, size}
-    console.log('HIGHTEST BID', highestBid)
+  const {price, sizeDiff} = bid
+  // console.log(price.toString(), sizeDiff.toString())
+  if(price.gt(highestBid.price) && sizeDiff.isPos()){
+    highestBid = {price, size: sizeDiff}
+    console.log(`HIGHEST BID: ${highestBid.price.toString()} ${highestBid.size.toString()}`)
   }
-  if(type == 'remove' && price == highestBid.price){
-    highestBid = {price: 0, size: 0}
+  else if(price.eq(highestBid.price)){
+    if(sizeDiff.isPos())
+      highestBid = {price, size: highestBid.size.add(sizeDiff)}
+    else
+      highestBid = {price, size: highestBid.size.add(sizeDiff)}
+    if(!highestBid.size.gt(0))
+      highestBid = {price: new Decimal(0), size: new Decimal(0)}
+    console.log(`HIGHEST BID: ${highestBid.price.toString()} ${highestBid.size.toString()}`)
   }
 
-  if(type=='add' && parseFloat(price) >= parseFloat(myOrderPair.ask.price) && myOrderPair.ask.state == 'open')
+  if(myOrderPair.ask.price && sizeDiff.isPos() && price.gte(myOrderPair.ask.price) && myOrderPair.ask.state == 'open')
     mockCompleteOrder({ask: bid})
 }
 
 function setAsk(ask){
-  let [price, type, size] = ask
-  price = parseFloat(price).toFixed(2)
-  type = type == 1 ? 'add' : 'remove'
-  // console.log('ask', price, parseFloat(price) , parseFloat(lowestAsk.price))
-  if(parseFloat(price) < parseFloat(lowestAsk.price) && type == 'add'){
-    lowestAsk = {price, size}
-    console.log('LOWEST ASK', lowestAsk)
+  const {price, sizeDiff} = ask
+  // console.log(price.toString(), sizeDiff.toString(), sizeDiff.isPos())
+  if(price.lt(lowestAsk.price) && sizeDiff.isPos()){
+    lowestAsk = {price, size: sizeDiff}
+    console.log(`LOWEST ASK: ${lowestAsk.price.toString()} ${lowestAsk.size.toString()}`)
   }
-  if(type == 'remove' && price == lowestAsk.price){
-    lowestAsk = {price: 100000, size: 0}
+  else if(price.eq(lowestAsk.price)){
+    if(sizeDiff.isPos())
+      lowestAsk = {price, size: lowestAsk.size.add(sizeDiff)}
+    else
+      lowestAsk = {price, size: lowestAsk.size.add(sizeDiff)}
+    if(!lowestAsk.size.gt(0))
+      lowestAsk = {price: new Decimal(100000), size: new Decimal(0)}
+    console.log(`LOWEST ASK: ${lowestAsk.price.toString()} ${lowestAsk.size.toString()}`)
   }
 
-  if(type=='add' && parseFloat(price) <= parseFloat(myOrderPair.bid.price) && myOrderPair.bid.state == 'open')
+  if(myOrderPair.bid.price && sizeDiff.isPos() && price.lte(myOrderPair.bid.price) && myOrderPair.bid.state == 'open')
     mockCompleteOrder({bid: ask})
 }
 
 function getOrderbookWS() {
   client.subscribeOrderbook(PAIR, '1E-2', (msg) => {
-    const { bids, asks } = msg.data
-    if(!!bids[0]) setBid(bids[0]);
-    if(!!asks[0]) setAsk(asks[0]);
+    if(msg.update){
+      const { bids, asks } = msg.update
+      if(!!bids[0]) setBid(bids[0]);
+      if(!!asks[0]) setAsk(asks[0]);
 
-    if(myOrderPair.bid.state=='filled' && myOrderPair.ask.state=='filled' &&
-      highestBid.size && lowestAsk.size)
-      createOrder()
+      if(myOrderPair.bid.state=='filled' && myOrderPair.ask.state=='filled' &&
+        !highestBid.price.eq(0) && !lowestAsk.price.eq(100000) && lastTrade.price)
+        createOrder()
+    }
   })
   .then(() => {
     console.error('subscribe orderbook')
@@ -72,12 +84,32 @@ function getOrderbookWS() {
   })
 }
 
+function getTradeWS() {
+  client.subscribePublicTrade(PAIR, (msg) => {
+    if(msg.update){
+      const{ price, size } = msg.update[0]
+      lastTrade = {price, size}
+      console.log(`\nTRADE OCCER at ${price.toString()} for ${size.toString()}\nbid: ${highestBid.price.toString()}, ask: ${lowestAsk.price.toString()}\n`);
+    }
+  })
+  .then(() => {
+    console.log('subscribe trade')
+  })
+  .catch((err) => {
+    console.log(err)
+    client.close()
+  })
+}
+
 
 function getOrderWS() {
   client.subscribeOrder((order) => {
-    const {price, size, state, side} = order.update
-    myOrderPair[side]={price: price.toFixed(2), size: size.toString(), state}
-    console.log('update myOrderPair', myOrderPair)
+    console.log(order);
+    if(order.update){
+      const {price, size, state, side} = order.update
+      myOrderPair[side]={price: price, size, state}
+      console.log('update myOrderPair', myOrderPair)
+    }
   })
   .then(()=>{
     console.log('subscribe order')
@@ -87,33 +119,32 @@ function getOrderWS() {
   })
 }
 
-client.on('open', ()=>{
-  console.error('open')
-  getOrderWS()
-  getOrderbookWS()
-})
+async function createOrder() {
+  // let dBid = lastTrade.price.sub(highestBid.price)
+  // let dAsk = lowestAsk.price.sub(lastTrade.price)
+  // let nearPrice = dBid.lt(dAsk) ? highestBid.price : lowestAsk.price
+  // let [higherPrice, lowerPrice] = lastTrade.price.gt(nearPrice) ? [lastTrade.price, nearPrice] : [nearPrice, lastTrade.price]
+  // let bidOrder = {side: 'bid', price: lowerPrice.add(0.01), size: ORDER_SIZE }
+  // let askOrder = {side: 'ask', price: higherPrice.sub(0.01), size: ORDER_SIZE }
 
+  let askOrder = {side: 'ask', price: lastTrade.price.add(0.01), size: ORDER_SIZE }
+  let bidOrder = {side: 'bid', price: lastTrade.price.sub(0.01), size: ORDER_SIZE }
 
-
-function cancelAllOrder() {
-  for(order of allOrders){
-    client.cancelOrder(order.id)
-  }
+  let realAsk = await placeOrder(askOrder.side, askOrder.price, askOrder.size)
+  let realBid = await placeOrder(bidOrder.side, bidOrder.price, bidOrder.size)
+  console.log('\nPLACE ORDER')
+  console.log(`ask: ${realAsk.price.toString()} ${realAsk.size.toString()} ${realAsk.state}`)
+  console.log(`bid: ${realBid.price.toString()} ${realBid.size.toString()} ${realBid.state}`)
+  // console.log('bid', bidOrder.price.toString(), bidOrder.size)
+  // myOrderPair[realBid.side]={price: realBid.price, size: realBid.size, state: realBid.state}
+  // console.log('ask', askOrder.price.toString(), askOrder.size)
+  // myOrderPair[realAsk.side]={price: realAsk.price, size: realAsk.size, state: realAsk.state}
+  console.log('\n')
+  process.exit(0)
 }
 
-function createOrder() {
-  let bidOrder = {side: 'bid', price: (parseFloat(highestBid.price) + 0.01).toFixed(2), size: ORDER_SIZE }
-  let askOrder = {side: 'ask', price: (parseFloat(lowestAsk.price) - 0.01).toFixed(2), size: ORDER_SIZE }
-
-  // client.placeLimitOrder(PAIR, 'bid', bidOrder.price, bidOrder.size, 'exchange')
-  // client.placeLimitOrder(PAIR, 'ask', askOrder.price, askOrder.size, 'exchange')
-  console.log('\n')
-  console.log(PAIR, 'bid', bidOrder.price, bidOrder.size, 'exchange')
-  myOrderPair[bidOrder.side]={price: bidOrder.price, size: bidOrder.size, state: 'open'}
-  console.log(PAIR, 'ask', askOrder.price, askOrder.size, 'exchange')
-  myOrderPair[askOrder.side]={price: askOrder.price, size: askOrder.size, state: 'open'}
-  console.log('\n')
-
+function placeOrder(side, price, size){
+  return client.placeLimitOrder(PAIR, side, price, size, 'exchange')
 }
 
 function mockCompleteOrder({bid, ask}) {
@@ -126,9 +157,17 @@ function mockCompleteOrder({bid, ask}) {
     console.log(`SELL at ${myOrderPair.ask.price}`)
   }
   if(myOrderPair.bid.state == myOrderPair.ask.state){
-    console.log(`\nEARN: ${parseFloat(myOrderPair.ask.price) - parseFloat(myOrderPair.bid.price) * ORDER_SIZE}`)
+    console.log(`\nEARN: ${myOrderPair.ask.price.sub(myOrderPair.bid.price).mul(ORDER_SIZE).toString()}`)
   }
 }
+
+
+client.on('open', ()=>{
+  console.error('open')
+  getOrderWS()
+  getOrderbookWS()
+  getTradeWS()
+})
 
 /*
 就是我刚开始编写机器人的源代码，几乎没有改动，参数也是原来的参数。这个版本的程序有许多
